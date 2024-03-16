20584ac33b5e269e1420ed8dd0d2094d9097422b
EricCMcAfee <eric.c.mcafee@gmail.com>
2024-03-16 10:24:50 -0600

    update on 3/16

--- Test.cpp
+++ Test.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
+#include <iomanip>
+
 using namespace std;
+
 int main()
 {
-    cout << "Hello Worlds" << endl;
-    return 0;
+   int width;
+   int height;
+   int number_of_windows;
+   int window_area = 6;
+   cout << "Wall width: " << endl;
+   cout << "Wall height: " << endl;
+   cout << "Number of windows: " << endl;
+   cin >> width >> height >> number_of_windows;
+   cout << "Area: " << height * width - number_of_windows * window_area << endl;
+   
+   
+   return 0;
 }
